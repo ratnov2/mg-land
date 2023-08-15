3f750b4fd55a13aff3e3331b06a51fa0d3b2445a
Eeddevv <edscience.commercial@gmail.com@>
2023-08-15 16:52:32 +0300

    v1

---
+++ script.js
@@ -0,0 +1,113 @@
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".top-navigation");
+const navigation = document.querySelector("header");
+
+console.log(menu);
+menu.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+  document.body.classList.toggle("lock");
+});
+
+const anim = document.querySelectorAll(".right-element li");
+const accordion = document.querySelectorAll(".right-element li p");
+const svg = document.querySelectorAll(".right-element li .svg");
+const leftElementH4 = document.querySelector(".left-element h4");
+const leftElementP = document.querySelector(".left-element p");
+
+let number = 0;
+
+//accordion
+
+for (let i = 0; i < anim.length; i++) {
+  accordion[0].style.maxHeight = accordion[0].scrollHeight + "px";
+  anim[i].addEventListener("click", () => {
+    accordion[i].style.maxHeight = accordion[i].scrollHeight + "px";
+    console.log(number, i);
+    if (number !== i && anim[number].classList.contains("accordion-active")) {
+      accordion[number].style.maxHeight = "0";
+      anim[number].classList.remove("accordion-active");
+    } else if (
+      number === i &&
+      !anim[number].classList.contains("accordion-active")
+    ) {
+      anim[i].classList.add("accordion-active");
+    } else if (
+      number === i &&
+      anim[number].classList.contains("accordion-active")
+    ) {
+      accordion[number].style.maxHeight = "0";
+      anim[i].classList.remove("accordion-active");
+    }
+
+    if (i !== number) anim[i].classList.add("accordion-active");
+
+    number = i;
+    leftElementH4.innerHTML = data[i].title;
+    leftElementP.innerHTML = data[i].text;
+  });
+}
+
+const data = [
+  {
+    text: " Каучуковая формула, эластичность которой, в отличие от других баз,даёт долгий срок носки без сколов и трещин – от 3-4 недель иболее.",
+    title: "Как долго держится база?",
+  },
+  {
+    text: " Каучуковая формула, эластичность которой, в отличие от других баз,даёт долгий срок носки без сколов и трещин – от 3-4 недель иболее.",
+    title: "Для какого типа ногтей подходит?",
+  },
+  {
+    text: " Каучуковая формула, эластичность которой, в отличие от других баз,даёт долгий срок носки без сколов и трещин – от 3-4 недель иболее.",
+    title: "Требуется ли наносить подложку? ",
+  },
+  {
+    text: " Каучуковая формула, эластичность которой, в отличие от других баз,даёт долгий срок носки без сколов и трещин – от 3-4 недель иболее.",
+    title: "Как долго держится база?",
+  },
+  {
+    text: " Каучуковая формула, эластичность которой, в отличие от других баз,даёт долгий срок носки без сколов и трещин – от 3-4 недель иболее.",
+    title: "Требуется ли наносить подложку? ",
+  },
+  {
+    text: " Каучуковая формула, эластичность которой, в отличие от других баз,даёт долгий срок носки без сколов и трещин – от 3-4 недель иболее.",
+    title: "Как долго держится база?",
+  },
+];
+
+//slider
+
+const prev = document.querySelector(".slider-prev");
+const next = document.querySelector(".slider-next");
+const slider = document.querySelector(".slider");
+const items = document.querySelectorAll(".slider-item");
+
+currentItem = 0;
+currentWidth = 0;
+
+next.addEventListener("click", () => {
+  let width = items[0].getBoundingClientRect().width + 40;
+  if (currentItem !== items.length - 1) {
+    items[currentItem].style.transform = `translateX(${
+      currentWidth - width
+    }px)`;
+    for (let i = currentItem + 1; i < items.length; i++) {
+      items[i].style.transform = `translateX(${currentWidth - width}px)`;
+    }
+    currentWidth -= width;
+    currentItem++;
+  }
+});
+
+prev.addEventListener("click", () => {
+  const width = items[0].getBoundingClientRect().width + 40;
+  if (currentItem !== 0) {
+    items[currentItem].style.transform = `translateX(${
+      currentWidth + width
+    }px)`;
+    for (let i = currentItem - 1; i >= 0; i--) {
+      items[i].style.transform = `translateX(${currentWidth + width}px)`;
+    }
+    currentWidth += width;
+    currentItem--;
+  }
+});
